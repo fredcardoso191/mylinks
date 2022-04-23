21492eacbfafec1179e118b25ea59ade186f45e5
Fred Cardoso <fredcardoso01@hotmail.com>
2022-04-23 16:55:22 -0300

    my links

---
+++ script.js
@@ -0,0 +1,10 @@
+let copyEmail = () => {
+    const email = "recco.fred01@hotmail.com";
+    let inputTest = document.createElement("input");
+    inputTest.value = email;
+    document.body.appendChild(inputTest);
+    inputTest.select();
+    document.execCommand('copy');
+    document.body.removeChild(inputTest);
+    alert("Email successfully copied: " + email)
+};
